3722fabd3c7e231a4dbf24dc330ddd5154dda417
parisa <parisafarivash@gmail.com>
2023-01-10 20:16:23 +0330

    Changed name of app

--- jira/settings.py
+++ jira/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
 
-    'dolphin',
+    'src',
     'user',
 ]
 
--- jira/urls.py
+++ jira/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('user.urls')),
-    path('', include('dolphin.urls')),
+    path('', include('src.urls')),
 ]
