99b2595ab09df8b2225b11cbe127e260cb078cd3
parisa <parisafarivash@gmail.com>
2024-08-04 08:23:48 +0330

    Added action to admin panel

--- analytics/admin.py
+++ analytics/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import ObjectViewed
 
-# Register your models here.
-admin.site.register(ObjectViewed)
+
+@admin.register(ObjectViewed)
+class ObjectViewAdmin(admin.ModelAdmin):
+    list_display = ['ip_address', 'object_id', 'content_object', 'timestamp']
+
+    list_select_related = ['content_type', 'user']
+    readonly_fields = ['object_id', 'content_object', 'content_type']
+    list_filter = ['user', 'timestamp']
+    list_search = ['user']
+    actions = ['update']
+
+    @admin.action(description='Updated selected ip')
+    def update(self, request, queryset):
+        queryset.update(ip_address='localhost')
 
